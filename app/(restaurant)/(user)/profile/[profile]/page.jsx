23a573ed58fca0acf7ed038390e0461f58a5165c
Zeynep Ünal <zbusra.unal@gmail.com>
2023-09-27 17:41:19 +0300

    create: restaurant page

---
+++ app/(restaurant)/(user)/profile/[profile]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '/app/(restaurant)/(user)/profile/[profile]/style.module.css';
+import RestaurantLayout from '../../../layout';
+
+export default function Profile({ params }) {
+  return (
+    <RestaurantLayout>
+      <div className={'container fluid'}>
+        My Profile Page - My Profile Page - My Profile Page - My Profile Page
+      </div>
+    </RestaurantLayout>
+  );
+}
