6431b9cb14022953636dc457ab4e99542afa3cd9
Zeynep Ünal <zbusra.unal@gmail.com>
2023-09-27 20:33:27 +0300

    refactor: align reservation page structure

--- app/(restaurant)/(admin)/restaurants/[restaurantName]/manage/page.jsx
+++ app/(restaurant)/(admin)/restaurants/[restaurantName]/manage/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from '/app/(restaurant)/(user)/profile/[profile]/style.module.css';
-import RestaurantLayout from '../../../layout';
+import styles from './style.module.css';
+import RestaurantLayout from '/app/(restaurant)/layout';
 
 export default function Profile({ params }) {
   return (
