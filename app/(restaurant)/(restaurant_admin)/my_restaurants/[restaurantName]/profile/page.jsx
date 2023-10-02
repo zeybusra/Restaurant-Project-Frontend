98dee98b4b71a87c6cee9704afd0cd7e6139065d
Zeynep Ünal <zbusra.unal@gmail.com>
2023-10-03 00:57:44 +0300

    feat: design profile page

--- app/(restaurant)/(restaurant_admin)/my_restaurants/[restaurantName]/profile/page.jsx
+++ app/(restaurant)/(restaurant_admin)/my_restaurants/[restaurantName]/profile/page.jsx
@@ -6,6 +6,7 @@ import {PiPhoneCallLight} from 'react-icons/pi';
 import {CiMail} from 'react-icons/ci';
 import {SlLocationPin} from 'react-icons/sl';
 import {MdOutlineAddAPhoto} from 'react-icons/md';
+import {RxInfoCircled} from "react-icons/rx";
 import restaurantImage1 from '/components/image/restaurant-logo-kirpi.png';
 import {Input} from '@nextui-org/react';
 import Link from "next/link";
@@ -18,37 +19,70 @@ export default function Profile({params}) {
   }
 
   return (
-    <div className={'container'}>
+    <div className={styles.container}>
 
       <div className={styles.cardContainer}>
+        <div className={styles.editRestaurantInfo}>
+          <RxInfoCircled className={styles.infoAboutIcon}/>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <span style={{fontWeight: "600"}}>Restaurant Bilgilerini Girin.</span>
+            <span>Lütfen restaurant bilgilerini eksiksiz ve doğru giriniz. Bu bilgiler aynı zamanda menü ekranında gösterilecektir.</span>
+          </div>
+        </div>
+
         <h2 className={styles.restaurantTitle}>Restaurant Bilgileri</h2>
 
-        <div className={styles.cardHeader}>
-
-          <Input value={"lorem"} placeholder={"Restaurant ismini giriniz..."}/>
-        </div>
         <div className={styles.cardBody}>
 
           <div style={{marginTop: "30px"}}>
 
-            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "15px"}}>
-              <MdOutlineAddAPhoto className={styles.cardIcon} />
-              <Input type={"file"} placeholder={"Profil resmi seçiniz..."}/>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "35px"}}>
+              <Input placeholder={"Restaurant ismini giriniz..."} label="Restaurant İsmi" value={"KİRPİ / SUADİYE"} labelPlacement={"outside"} size={`lg`}/>
+
             </div>
 
-            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "15px"}}>
-              <SlLocationPin className={styles.cardIcon}/>
-              <Input placeholder={"Adres bilgilerini giriniz..."}/>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "35px"}}>
+              {/*<MdOutlineAddAPhoto className={styles.cardIcon}/>*/}
+              <Input type={"file"} placeholder={"Profil resmi seçiniz..."} label={"Logo"} labelPlacement={"outside"}  size={`lg`}/>
             </div>
 
-            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "15px"}}>
-              <PiPhoneCallLight className={styles.cardIcon}/>
-              <Input value={"lorem"} placeholder={"Telefon bilgilerini giriniz..."}/>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent:"space-between", marginBottom:"35px"}}>
+              <div style={{width:"45%"}}>
+                {/*<PiPhoneCallLight className={styles.cardIcon}/>*/}
+                <Input placeholder={"Adres bilgilerini giriniz..."} label="Adres" labelPlacement={"outside"}  size={`lg`}/>
+              </div>
+
+              <div style={{width:"45%"}}>
+                <Input placeholder={"Wifi Şifresini giriniz..."} label="Wifi Şifresi" labelPlacement={"outside"}  size={`lg`}/>
+              </div>
             </div>
 
-            <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "15px"}}>
-              <CiMail className={styles.cardIcon}/>
-              <Input value={"lorem"} placeholder={"Mail bilgilerini giriniz..."}/>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent:"space-between", marginBottom:"35px"} }>
+              <div style={{width:"45%"}}>
+                {/*<PiPhoneCallLight className={styles.cardIcon}/>*/}
+                <Input className={styles.foodItemInput} placeholder={"Telefon bilgilerini giriniz..."}
+                       label="Telefon" labelPlacement={"outside"}  size={`lg`}></Input>
+              </div>
+
+              <div style={{width:"45%"}}>
+                {/*<CiMail className={styles.cardIcon}/>*/}
+                <Input className={styles.foodItemInput} placeholder={"Mail bilgilerini giriniz..."}
+                       label="Mail" labelPlacement={"outside"}  size={`lg`}></Input>
+              </div>
+            </div>
+
+
+
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent:"space-between", marginBottom:"35px"}}>
+              <div style={{width:"45%"}}>
+                <Input className={styles.foodItemInput} placeholder={"link veya kullanıcı bilgisi giriniz..."}
+                       label="Instagram" labelPlacement={"outside"} size={`lg`}></Input>
+              </div>
+
+              <div style={{width:"45%"}}>
+                <Input className={styles.foodItemInput} placeholder={"link veya kullanıcı bilgisi giriniz..."}
+                       label="Facebook" labelPlacement={"outside"} size={`lg`}></Input>
+              </div>
             </div>
 
             <div className={styles.formButtons}>
