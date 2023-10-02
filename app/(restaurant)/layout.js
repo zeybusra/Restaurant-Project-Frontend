4aafb79c3a31baeda519b1368199c47d07a7086e
Zeynep Ünal <zbusra.unal@gmail.com>
2023-10-02 15:44:17 +0300

    feat: update sidebar

--- app/(restaurant)/layout.js
+++ app/(restaurant)/layout.js
@@ -1,15 +1,29 @@
+"use client";
 import React from 'react';
 import styles from '/app/(restaurant)/style.module.css';
 import Header from 'components/header';
 import Footer from 'components/footer';
-import SidebarTemplate from '../../components/sidebar';
+import SidebarTemplate from '/components/sidebar';
 import Image from 'next/image';
 import appLogo from '../../components/image/waiter-call-icon-black.png';
-import { IoIosNotificationsOutline } from 'react-icons/io';
-import { PiMoonLight } from 'react-icons/pi';
-import profilePic from '../../components/image/profile-picture-1.png';
+import {IoIosNotificationsOutline} from 'react-icons/io';
+import {PiMoonLight} from 'react-icons/pi';
+import profilePic from '/components/image/profile-picture-1.png';
+import {usePathname} from "next/navigation";
 
-export default function RestaurantLayout({ children }) {
+export default function RestaurantLayout({children}) {
+
+  console.log('-'.repeat(10));
+  const path = usePathname()
+  const lastPath = path.split('my_restaurants')[1];
+  let sidebarType;
+  if (lastPath?.length > 1) {
+    sidebarType = '2';
+  } else {
+    sidebarType = '1';
+  }
+  console.log('sidebarType', sidebarType);
+
   return (
     <div className={styles.gridTemplate}>
       {/*<Header className={styles.mainHeader} />*/}
@@ -28,11 +42,11 @@ export default function RestaurantLayout({ children }) {
 
         <div className={styles.navbarRightContainer}>
           <div className={styles.navbarRight}>
-            <IoIosNotificationsOutline className={styles.navbarRightImg} />
+            <IoIosNotificationsOutline className={styles.navbarRightImg}/>
           </div>
 
           <div className={styles.navbarRight}>
-            <PiMoonLight className={styles.navbarRightImg} />
+            <PiMoonLight className={styles.navbarRightImg}/>
           </div>
 
           <div className={styles.navbarRight}>
@@ -48,11 +62,11 @@ export default function RestaurantLayout({ children }) {
       </div>
       {/*HEADER COMPONENT CONTENT*/}
 
-      <SidebarTemplate className={styles.sidebar} />
+        <SidebarTemplate className={styles.sidebar} sidebarType={sidebarType}/>
 
       <div className={styles.content}>{children}</div>
 
-      <Footer className={styles.mainFooter} />
+      <Footer className={styles.mainFooter}/>
     </div>
   );
 }
--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -8,27 +8,108 @@ import {VscGraphLine} from "react-icons/vsc";
 import {TbQuestionMark} from "react-icons/tb";
 import {LiaHandshake} from "react-icons/lia";
 
-const drawerWidth = 240;
+function SidebarTemplate({children, sidebarType}) {
 
-function SidebarTemplate({children}) {
+  const sidebarDetailContent = [
+    {
+      name: "Profil Ayarları",
+      icon: <IoRestaurantOutline className={styles.iconStyle}/>,
+      link: "/my_restaurants/kirpi/profile"
+    },
+    {
+      name: "Menü Ayarları",
+      icon: <BsPersonGear className={styles.iconStyle}/>,
+      link: "/my_restaurants/kirpi/menu"
+    },
+    {
+      name: "Rezervasyonlarım",
+      icon: <VscGraphLine className={styles.iconStyle}/>,
+      link: "/my_restaurants/kirpi/reservations"
+    },
+    {
+      name: "Analizlerim",
+      icon: <VscGraphLine className={styles.iconStyle}/>,
+      link: "/my_restaurants/kirpi/analysis"
+    }
+  ];
+
+
+  const sidebarAdminManageContent = [
+    {
+      name: "Restaurantlarım",
+      icon: <IoRestaurantOutline className={styles.iconStyle}/>,
+      link: "/restaurants/ledkfjldf/manage"
+    },
+    {
+      name: "Profilim",
+      icon: <BsPersonGear className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Analizlerim",
+      icon: <VscGraphLine className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Sıkça Sorulan Sorular",
+      icon: <TbQuestionMark className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Yardım",
+      icon: <LiaHandshake className={styles.iconStyle}/>,
+      link: "/"
+    }
+  ]
+
+
+  const sidebarDefaultContent = [
+    {
+      name: "Restaurantlarım",
+      icon: <IoRestaurantOutline className={styles.iconStyle}/>,
+      link: "/restaurants/ledkfjldf/manage"
+    },
+    {
+      name: "Profilim",
+      icon: <BsPersonGear className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Analizlerim",
+      icon: <VscGraphLine className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Sıkça Sorulan Sorular",
+      icon: <TbQuestionMark className={styles.iconStyle}/>,
+      link: "/"
+    },
+    {
+      name: "Yardım",
+      icon: <LiaHandshake className={styles.iconStyle}/>,
+      link: "/"
+    }
+  ]
+
+  const sidebarMap = {
+    '1': sidebarDefaultContent,
+    '2': sidebarDetailContent,
+    '3': sidebarAdminManageContent
+  }
+
   return (
     <div className={styles.sidebar}>
-      <Link href={"/restaurants/ledkfjldf/manage"}>
-        <IoRestaurantOutline className={styles.iconStyle}/>
-        Restaurantlarım</Link>
-      <Link href={"/"}>
-        <BsPersonGear className={styles.iconStyle}/>
-        Profili Düzenle</Link>
-      <Link href={"/"}>
-        <VscGraphLine className={styles.iconStyle}/>
-        Analizlerim</Link>
-      <Link href={"/"}>
-        <TbQuestionMark className={styles.iconStyle}/>
-        Sıkça Sorulan Sorular</Link>
-      <Link href={"/"}>
-        <LiaHandshake className={styles.iconStyle}/>
-        Yardım</Link>
-      {/*<Link href={"/"}>Restaurantlarım</Link>*/}
+      <span style={{textAlign:"center", fontSize:"18px", marginTop:"20px", marginBottom:"10px"}}>KİRPİ</span>
+      <hr></hr>
+
+      {sidebarMap[sidebarType].map((item, index) => {
+        return (
+          <Link href={item.link} key={index}>
+            {item.icon}
+            {item.name}
+          </Link>
+        );
+      })}
     </div>
 
   );
