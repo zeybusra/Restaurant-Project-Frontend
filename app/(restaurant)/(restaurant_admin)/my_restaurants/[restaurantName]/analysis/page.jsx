e25c49d3502a022450b0acf3914dcb0dc1bbfc3d
Zeynep Ünal <zbusra.unal@gmail.com>
2023-10-09 16:43:53 +0300

    feat: design analysis page

--- app/(restaurant)/(restaurant_admin)/my_restaurants/[restaurantName]/analysis/page.jsx
+++ app/(restaurant)/(restaurant_admin)/my_restaurants/[restaurantName]/analysis/page.jsx
@@ -6,9 +6,74 @@ export default function Analysis({params}) {
 
 
   return (
-    <div className={'container'}>
+    <div>
+      <div className={styles.gridContainer}>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+      </div>
 
 
+      <div className={styles.gridContainer2}>
+        <div className={styles.container}>
+        <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+      </div>
+
+
+      <div className={styles.gridContainer}>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+        <div className={styles.container}>
+          <div>En Çok Tercih Edilen Yemek</div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam dolor dolore excepturi iure
+          possimus, quae quas quo recusandae reprehenderit similique sunt! A accusamus ad animi aperiam beatae,
+          consectetur culpa cupiditate deleniti excepturi inventore ipsum iste laboriosam molestiae sed soluta ut vel!
+          Accusantium aliquam, consectetur itaque nisi officia officiis tempora?
+        </div>
+      </div>
     </div>
   );
 }
